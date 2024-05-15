8af0be5da490ce780711ec1d86a3be18239f884a
tienbt <tientt8436@gmail.com>
2024-05-15 15:28:45 +0700

    Commit A1

--- src/pages/git/index.tsx
+++ src/pages/git/index.tsx
@@ -4,16 +4,8 @@ const Git = () => {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Git</h1>
-
-      <p>Commit develop 1</p>
-      <p>Commit develop 2</p>
-      <p>Commit html 2</p>
-      <p>Commit html 3</p>
-
       <div>
-        <p>This is learn git 2</p>
-        <p>Commit v2 html 1</p>
-        <p>Commit v2 html 2</p>
+        <p>Commit A1</p>
       </div>
     </div>
   );
