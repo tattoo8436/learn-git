24ed0b4db279da8a9e20d254ee7c1772cfdc5bcf
tienbt <tientt8436@gmail.com>
2024-05-16 13:36:48 +0700

    E1

--- src/pages/git/index.tsx
+++ src/pages/git/index.tsx
@@ -5,10 +5,7 @@ const Git = () => {
     <div style={{ padding: "2rem" }}>
       <h1>Git</h1>
       <div>
-        <p>C1</p>
-        <p>C2</p>
-        <p>C3</p>
-        <p>C4</p>
+        <p>E1</p>
       </div>
     </div>
   );
