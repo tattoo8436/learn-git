a7ca94f2040a67b4ede94b48435006a67d9dd365
tienbt <tientt8436@gmail.com>
2024-12-10 14:03:56 +0700

    1.1

--- src/pages/git/index.tsx
+++ src/pages/git/index.tsx
@@ -1,22 +1,11 @@
 import "./styles.css";
 
 const Git = () => {
-  const test1 = () => {
-    console.log("1");
-    console.log("2");
-    console.log("3");
-  };
-
-  const test2 = () => {
-    console.log("145");
-  };
-
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Git</h1>
       <div>
-        <p>1.23</p>
-        <p>2</p>
+        <p>1.1</p>
       </div>
     </div>
   );
